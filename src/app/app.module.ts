4ede6570040283fdfc0bc594f7b75f1dbf02abce
phanitumuluri-rally <tumuluri.phani@rallyhealth.com>
2023-04-23 21:19:28 +0530

    generated build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
-import { NgModule, isDevMode } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WodComponent } from './components/wod/wod.component';
 import { ExerciseListComponent } from './components/exercise-list/exercise-list.component';
@@ -19,12 +18,6 @@ import { SafeUrlPipe } from './pipes/safe-url.pipe';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    }),
     BrowserAnimationsModule
   ],
   providers: [],
